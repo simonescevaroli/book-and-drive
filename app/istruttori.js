daef280d555c7d8446cfad10351bc5688e1d8e9f
Andrea Lorenzetti <lorenzetti889@gmail.com>
2022-05-14 16:23:03 +0200

    retrieve id of all istructors and booked istructors

--- app/istruttori.js
+++ app/istruttori.js
@@ -11,8 +11,23 @@ router.get('/visualizzaImpegni',(req,res)=>{
     //Visualizza le guide prenotate con me (istruttore)
 });
 
-router.get('/verificaDiponibilita',(req,res)=>{
+router.get('/verificaDiponibilita',async (req,res)=>{
     //Visualizza le guide prenotate con tutti gli istruttori
+    var slot=new Date(req.body.slot);
+    
+    var all_istructors= await Istruttore.find({},{_id:1}).exec();
+    for(let i=0; i < all_istructors.length; i++){
+        all_istructors[i]=all_istructors[i]._id.toString();
+    }
+    console.log(all_istructors);
+
+    
+    const booked_istructors= await Prenotazione.find({slot: slot},{_id: 0, id_istruttore:1}).exec();
+    for(let i=0; i < booked_istructors.length; i++){
+        booked_istructors[i]=booked_istructors[i].id_istruttore;
+    }
+    console.log(booked_istructors);
+
 });
     
 
