c7c03b1af3cf8f77d6f42d057b04253821e610bb
elisacomposta <98694899+elisacomposta@users.noreply.github.com>
2022-05-15 11:04:47 +0200

    add visualization of all students

--- app/segreteria.js
+++ app/segreteria.js
@@ -11,8 +11,24 @@ router.get('/guideStudenti',(req,res)=>{
     //Visualizza le guide prenotate di tutti gli studenti
 });
 
-router.get('/resocontoStudenti',(req,res)=>{
-    //Visualizza i dati personali di tutti gli studenti
+
+
+// Visualizza dati personali di tutti gli studenti
+router.get('/resocontoStudenti', async (req,res)=>{
+    elenco_studenti = await Studente.find({});
+
+    elenco_studenti = elenco_studenti.map( (stud) => {
+        return {
+            self: '/api/v1/studenti/' + stud.id,
+            foglio_rosa: stud.username,
+            nome: stud.nome,
+            cognome: stud.cognome,
+            dataNascita: stud.dataNascita.toLocaleString('it-IT'),
+            email: stud.email,
+            telefono: stud.telefono
+        };
+    });
+    res.status(200).json(elenco_studenti);
 });
 
 
