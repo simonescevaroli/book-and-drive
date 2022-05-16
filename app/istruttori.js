30c2bcae615658b34ab1f0bf7f1a827b60914cd5
Andrea Lorenzetti <lorenzetti889@gmail.com>
2022-05-16 11:09:42 +0200

    reset my apis

--- app/istruttori.js
+++ app/istruttori.js
@@ -11,48 +11,8 @@ router.get('/visualizzaImpegni',(req,res)=>{
     //Visualizza le guide prenotate con me (istruttore)
 });
 
-router.get('/verificaDiponibilita',async (req,res)=>{
-    //Visualizza le guide prenotate con tutti gli istruttori
-    var slot=new Date(req.body.slot);
-    console.log("slot in verifica disponibilita:",slot);
-    //recupero gli id di tutti gli istruttori nel db
-    var all_istructors= await Istruttore.find({},{_id:1}).exec();
-    for(let i=0; i < all_istructors.length; i++){
-        all_istructors[i]=all_istructors[i]._id.toString();
-    }
-    console.log(all_istructors);
-
-    //recupero gli id di tutti gli istruttori già prenotati per quella data e ora
-    const booked_istructors= await Prenotazione.find({slot: slot},{_id: 0, id_istruttore:1}).exec();
-    for(let i=0; i < booked_istructors.length; i++){
-        booked_istructors[i]=booked_istructors[i].id_istruttore;
-    }
-    console.log(booked_istructors);
-
-    //salvo gli id degli istruttori non occupati per quello slot temporale
-    var available_istructors_id = [];
-    for(let i=0; i < all_istructors.length;i++){
-        if(!booked_istructors.includes(all_istructors[i])){
-            available_istructors_id.push(all_istructors[i]);
-        }
-    };
-    //recupero dati degli istruttori disponibili e li invio in risposta al client oppure invio l errore riscontrato
-    Istruttore.find({_id: {$in: available_istructors_id}},{_id:1,nome:1,cognome:1}).exec().then((data)=>{    
-        console.log("data:",data);
-        if(!data.length){
-            res.status(204).json({message:"non ci sono istruttori disponibili per quello slot"});
-        }
-        else{
-            res.status(200).json({
-                available_istructors: data
-            });
-        }
-    })
-    .catch((err)=>{ 
-        console.log("errore:"+err);
-        res.status(404).json({error: ''+err});
-    });
-
+router.get('/verificaDiponibilita',(req,res)=>{
+    //vierifica disponibilità degli istruttori
 });
     
 
--- app/prenotazioni.js
+++ app/prenotazioni.js
@@ -10,39 +10,10 @@ const Segreteria = require('./models/segreteria.js');
 
 router.post('/prenotaGuida', (req,res)=>{
     //Prenota una guida
-    var slot = new Date(req.body.slot);//YYYY-MM-DDTHH:mm
-    slot.setHours(slot.getHours() + 2);
-    console.log("prenotazione guida in data:",slot);//qui stampa 2 ore in meno
-    var prenotazione = new Prenotazione({
-        slot: slot,
-        id_studente: req.query.id_studente,
-        id_istruttore: req.body.id_istruttore,
-    });
-    console.log(prenotazione);
-    
-    prenotazione.save()
-    .then((prenotazione)=>{
-        res.status(201).json({
-            self: '/api/v1/prentoazioni/:'+prenotazione._id,
-            message: "guida prenotata con successo"
-        });
-    })
-    .catch((err)=>{ 
-        console.log("errore:"+err);
-        res.status(404).json({error: ''+err});
-    });
 });
 
 router.delete('/annullaGuida', (req,res)=>{
-    console.log("annulla guida");
-    Prenotazione.deleteOne({_id: req.query._id})
-    .then(()=>{
-        console.log(res.status(200).json({message: "guida cancellata con successo"}));
-    })
-    .catch((err)=>{
-        res.status(500).json({
-            error: ''+err})
-    })
+    //annulla una prenotazione
 });
 
 
