0114a33a7bdd13bb6f725ca793c4c6394673e07f
Andrea Lorenzetti <lorenzetti889@gmail.com>
2022-05-16 18:52:54 +0200

    api verifica disponibilita completed

--- app/istruttori.js
+++ app/istruttori.js
@@ -12,7 +12,46 @@ router.get('/visualizzaImpegni',(req,res)=>{
 });
 
 router.get('/verificaDiponibilita',(req,res)=>{
-    //vierifica disponibilità degli istruttori
+    //verifica disponibilità degli istruttori
+    var slot=new Date(req.body.slot);
+    
+    //recupero gli id di tutti gli istruttori nel db
+    var all_istructors= await Istruttore.find({},{_id:1}).exec();
+    for(let i=0; i < all_istructors.length; i++){
+        all_istructors[i]=all_istructors[i]._id.toString();
+    }
+    console.log(all_istructors);
+
+    //recupero gli id di tutti gli istruttori già prenotati per quella data e ora
+    const booked_istructors= await Prenotazione.find({slot: slot},{_id:1 }).exec();
+    for(let i=0; i < booked_istructors.length; i++){
+        booked_istructors[i]=booked_istructors[i].id_istruttore;
+    }
+    console.log(booked_istructors);
+
+    //salvo gli id degli istruttori non occupati per quello slot temporale
+    var available_istructors_id = [];
+    for(let i=0; i < all_istructors.length;i++){
+        if(!booked_istructors.includes(all_istructors[i])){
+            available_istructors_id.push(all_istructors[i]);
+        }
+    };
+    //recupero dati degli istruttori disponibili e li invio in risposta al client oppure invio l errore riscontrato
+    Istruttore.find({_id: {$in: available_istructors_id}},{_id:1}).exec().then((data)=>{    
+        if(!data.length){
+            res.status(204).json({message:"non ci sono istruttori disponibili per quello slot"});
+        }
+        else{
+            res.status(200).json({
+                available_istructors: data
+            });
+        }
+    })
+    .catch((err)=>{ 
+        console.log("errore:"+err);
+        res.status(404).json({error: ''+err});
+    });
+
 });
     
 
