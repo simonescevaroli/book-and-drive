2803183cb28f24a6b940e90dd3309fbaf750b964
Andrea Lorenzetti <lorenzetti889@gmail.com>
2022-05-16 11:14:06 +0200

    reset all apis

--- app/studenti.js
+++ app/studenti.js
@@ -8,31 +8,8 @@ const Istruttore = require('./models/istruttore.js');
 const Segreteria = require('./models/segreteria.js');
 
 
-// visualizza dati personali studente
 router.get('/me',(req,res)=>{
-    const query = req.query;
-    const id = query.id
-
-    Studente.findById(id)
-    .then(profile =>{
-
-        res.status(200).json({
-            self: '/api/v1/studenti/' + profile.id,
-            foglio_rosa: profile.username,
-            nome: profile.nome,
-            cognome: profile.cognome,
-            dataNascita: profile.dataNascita.toLocaleString('it-IT'),   // time zone locale
-            telefono: profile.telefono,
-            email: profile.email
-        })
-    })
-    .catch(err =>{
-        res.status(400).json({
-            confirm: 'fail',
-            error: err.message,
-            description: 'Impossibile caricare dati personali'
-        })
-    })
+// visualizza dati personali studente
 });
 
 
