92fac72e7b9d8c334a0615dba167d4e133654bef
ZeniiL <luca.zeni@hotmail.it>
2022-05-16 20:35:07 +0200

    added visualizzaImpegni

--- app/istruttori.js
+++ app/istruttori.js
@@ -7,8 +7,38 @@ const Prenotazione = require('./models/prenotazione.js');
 const Istruttore = require('./models/istruttore.js');
 const Segreteria = require('./models/segreteria.js');
 
-router.get('/visualizzaImpegni',(req,res)=>{
+router.get('/visualizzaImpegni', async (req,res)=>{
     //Visualizza le guide prenotate con me (istruttore)
+    const query = req.query;
+    const istr = query.id;
+
+    try{
+        let prenotazioni = await Prenotazione.find({username_istruttore:istr}); //array of prenotazione with id_istruttore equal to istr
+        if(prenotazioni.length == 0){
+            res.status(200).json({
+                confirm: 'fail',
+                description: 'Nessuna prenotazione trovata'
+        })
+        } else {
+            prenotazioni = prenotazioni.map((prenot)=>{
+                return {
+                    self: "api/v1/prenotazioni/" + prenot.id,
+                    slot: prenot.slot.toLocaleString('it-IT'),
+                    studente: prenot.nominativo_studente,
+                    presenza: prenot.presenza
+                };
+            })
+            res.status(200).json(prenotazioni);
+        }
+    }
+    catch(err){
+        res.status(400).json({
+            confirm: 'fail',
+            error: err.message,
+            description: 'Impossibile caricare dati personali'
+        })
+    }
+
 });
 
 router.get('/verificaDiponibilita',(req,res)=>{
