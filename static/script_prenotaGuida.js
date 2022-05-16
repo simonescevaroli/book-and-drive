a290313a4ecb56d4b3ac9e74c9bde19e171a5edf
Andrea Lorenzetti <lorenzetti889@gmail.com>
2022-05-16 21:22:56 +0200

    able to recieve backend response

--- static/script_prenotaGuida.js
+++ static/script_prenotaGuida.js
@@ -4,15 +4,18 @@ function available_istructors_request()
     var form = document.getElementById("data_ora").elements;
     var data = form["data"].value;
     var ora = form["ora"].value;
-    var stringa_data_ora= data+"T"+ora;
-    alert(stringa_data_ora);
-    fetch('http://localhost:8080/api/v1/istruttori/verificaDiponibilita',{
+    var stringa_data_ora= data+"T"+ora+":00.000Z";
+    document.write(stringa_data_ora);
+    fetch("http://localhost:8080/api/v1/istruttori/verificaDiponibilita?slot="+stringa_data_ora,{
         method:"GET",
-
-        body:stringa_data_ora
-    }).then((res)=>{
-        console.log(resp);
-
-    }); 
+        headers: {
+            'Content-Type': 'application/json'
+          }
+    })
+    .then(response => response.json())
+    .then((res)=>{
+        console.log(res.available_istructors);    
+    });
+     
 };
  
