165e76262b79282216ee7413ec08cc6232e739b2
Andrea Lorenzetti <lorenzetti889@gmail.com>
2022-05-17 16:52:14 +0200

    front end delete prenotation

--- static/script_annullaGuida.js
+++ static/script_annullaGuida.js
@@ -1,8 +1,64 @@
-var my_id=""
+var my_id="foglio_rosa1679"
 
 visualizza_mie_guide();
 
 function visualizza_mie_guide(){
-    
+    var mie_prenotazioni=document.getElementById("visualizza guide");
+    fetch("http://localhost:8080/api/v1/prenotazioni/mieGuide?username_stud="+my_id,{
+        method:"GET",
+        headers: {
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Origin": "*"
+        }
+    })
+    .then((resp)=>{
+        console.log(resp.status);
+        var res=resp.json();
+        if(resp.status==404 || resp.status==500){
+            alert(res.message);
+            return;
+        }
+        return res;
+    })
+    .then((res)=>{
+        console.log(res);
+        console.log(res.length);
+        for(let i=0; i < res.length; i++){
+            var tag=document.createElement("p");
+            var textID=document.createTextNode("Guida con id: "+res[i].id_guida);
+            tag.appendChild(textID);
+            tag.appendChild(document.createElement("br"));
+            var textSlot=document.createTextNode("data: "+res[i].slot);
+            tag.appendChild(textSlot);
+            tag.appendChild(document.createElement("br"));
+            var textIstruct=document.createTextNode("istruttore: "+res[i].istruttore);
+            tag.appendChild(textIstruct);
+            tag.appendChild(document.createElement("br"));
+            mie_prenotazioni.appendChild(tag);
+        }
+    });
 }
 
+
+function annullaGuida(){
+    var box=document.getElementById("box")
+    var id_guida=box.value;
+    fetch("http://localhost:8080/api/v1/prenotazioni/annullaGuida?_id="+id_guida,{
+        method:"DELETE",
+        headers: {
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Origin": "*"
+        }
+    })
+    .then((resp)=>{
+        console.log(resp.status);
+        resp.json().then((res)=>{
+            if(resp.status==200){
+                alert(res.message);
+            }
+            else if(resp.status==500){
+                alert(res.error);
+            }
+        })
+    })
+}
