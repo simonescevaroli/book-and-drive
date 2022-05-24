bfee359aad4623b333849dd44f85cc78bfc7c5c0
Andrea Lorenzetti <lorenzetti889@gmail.com>
2022-05-24 11:14:58 +0200

    script_menuStudente created

---
+++ static/script_menuStudente.js
@@ -0,0 +1,16 @@
+
+function get_page_prenota(){
+    window.location="prenotaGuida.html"
+}
+function get_page_annulla(){
+    
+    window.location="annullaGuida.html"
+}
+function get_page_mieGuide(){
+    
+    window.location="mieGuide.html"    
+}
+function get_page_mieiDati(){
+    
+    window.location="mieiDati.html"
+}
