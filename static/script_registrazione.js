29b3dfc4ce87ab094d76524c48b001898c0701b0
simonescevaroli <simonescevaroli@gmail.com>
2022-05-24 11:44:22 +0200

    added registrazione istruttori UI and API

--- static/script_registrazione.js
+++ static/script_registrazione.js
@@ -41,11 +41,43 @@ function mandaRichiestaStudente(){
         }
         alert("Registrazione andata a buon fine, verrai redirezionato alla pagina di login...");
         // Un piccolo timer prima di essere redirezionato
-        await new Promise(r => setTimeout(r, 1500));
+        await new Promise(r => setTimeout(r, 1000));
         relocLogin();
     }))
 }
 
 function mandaRichiestaIstruttore(){
-    console.log("Istruttore registrato correttamente")
+    var form = document.getElementById("istrform").elements;
+    var nome = form["nome"].value;
+    var cognome = form["cognome"].value;
+    var password = form["password"].value;
+    var telefono = form["telefono"].value;
+    var _id = nome +"."+ cognome;
+    fetch("http://localhost:8080/api/v1/istruttori/nuovoIstruttore",{
+        method:"POST",
+        headers: {
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Origin": "*"
+        },
+        body:JSON.stringify({_id:_id, password: password, telefono: telefono})
+    })
+    .then((resp)=> resp.json()
+    .then(async (res)=>{
+        if(resp.status==500){
+            alert(res.error);
+            return;
+        }else if(resp.status==406){
+            alert(res.error);
+            return;
+        }
+        if(res.mod_user){
+            alert("ATTENZIONE!!!\nIl tuo username per accedere sarà\n\n"+res.username+"\n\nin quanto esiste un tuo omonimo.\nRegistrazione andata a buon fine, verrai redirezionato alla pagina di login...");
+        }else{
+            alert("Registrazione andata a buon fine, verrai redirezionato alla pagina di login...");
+        }
+        
+        // Un piccolo timer prima di essere redirezionato
+        await new Promise(r => setTimeout(r, 1000));
+        relocLogin();
+    }))
 }
