994f1f22d5d42c8add0168e9ef2f014278daf0fd
Andrea Lorenzetti <lorenzetti889@gmail.com>
2022-05-25 15:52:36 +0200

    UI mieGuide updated with token in request

--- static/script_mieGuide.js
+++ static/script_mieGuide.js
@@ -1,7 +1,22 @@
-var username_stud = "1q2w3e4r"
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+
 function mieGuide()
 {   
-    fetch("http://localhost:8080/api/v1/prenotazioni/mieGuide?username_stud="+username_stud,{
+    fetch("http://localhost:8080/api/v1/prenotazioni/mieGuide?token="+getCookie("token"),{
         method:"GET",
         headers: {
             'Content-Type': 'application/json',
