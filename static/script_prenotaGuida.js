c958a463895785f213a1eacfbcc30cfd30058a85
Andrea Lorenzetti <lorenzetti889@gmail.com>
2022-05-25 15:54:53 +0200

    UI prenotaGuida updated with token in request

--- static/script_prenotaGuida.js
+++ static/script_prenotaGuida.js
@@ -1,4 +1,19 @@
-var my_id="foglio_rosa07589";
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+
 function available_istructors_request()
 {   
     var form = document.getElementById("data_ora").elements;
@@ -6,7 +21,7 @@ function available_istructors_request()
     var ora = form["ora"].value;
     var stringa_data_ora= data+"T"+ora+":00.000Z";
     console.log(stringa_data_ora);
-    fetch("http://localhost:8080/api/v1/istruttori/verificaDiponibilita?slot="+stringa_data_ora,{
+    fetch("http://localhost:8080/api/v1/istruttori/verificaDiponibilita?token="+getCookie("token")+"&slot="+stringa_data_ora,{
         method:"GET",
         headers: {
             'Content-Type': 'application/json',
@@ -75,7 +90,7 @@ function effettua_prenotazione(){
 
 function invia_dati_per_prenotazione(slot,username_istruttore){
     console.log("username istruttore:",username_istruttore)
-    fetch("http://localhost:8080/api/v1/prenotazioni/prenotaGuida/?username_studente="+my_id,{
+    fetch("http://localhost:8080/api/v1/prenotazioni/prenotaGuida?token="+getCookie("token"),{
         method:"POST",
         headers: {
             'Content-Type': 'application/json',
@@ -86,11 +101,11 @@ function invia_dati_per_prenotazione(slot,username_istruttore){
     .then((resp)=>{
         resp.json().then((res)=>{
             if(resp.status==208){
-                alert(res.message+"\n"+"URI:"+res.self);
+                alert(res.message+"\n"+"URI:"+res.self+"\n");
                 location.reload();
             }
             else if(resp.status==201){
-                alert(res.message+'\nHai effettuato la prenotazione con slot orario:'+slot+" e istruttore: "+username_istruttore+'\n'+"ritorna al menù");
+                alert(res.message+'\nHai effettuato la prenotazione con slot orario:'+res.prenotazione.slot+" e istruttore: "+res.prenotazione.username_istruttore+'\n'+"ritorna al menù");
                 location.reload();
             }
             else if(resp.status==404){
