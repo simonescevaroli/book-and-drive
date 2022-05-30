093dbe6be81fe2c1443137c8aa3ace2b885c4fae
elisacomposta <98694899+elisacomposta@users.noreply.github.com>
2022-05-30 19:36:51 +0200

    set up UI prenotazioni

--- static/script_menuIstruttore.js
+++ static/script_menuIstruttore.js
@@ -3,11 +3,9 @@ function get_page_visualizzaImpegni(){
     window.location="visualizzaImpegni.html"
 }
 function get_page_registroPresenze(){
-    
-    alert("funzionalità non ancora implementata")   
+    window.location="registroPresenze.html"
 }
 function get_page_modDisponibilita(){
-    
     alert("funzionalità non ancora implementata")
 }
 function get_help_istruttore(){
---
+++ static/script_registroPresenze.js
@@ -0,0 +1,22 @@
+function loadPrenotazioni(){
+}
+
+
+function back(){
+  window.location="menu_istruttore.html"
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
