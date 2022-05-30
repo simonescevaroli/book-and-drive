8e9a572dd58d7174350c9d21cfd47b29b425a71e
ZeniiL <luca.zeni@hotmail.it>
2022-05-30 20:52:22 +0200

    added function loadPrenotazioni

--- static/script_registroPresenze.js
+++ static/script_registroPresenze.js
@@ -1,4 +1,86 @@
 function loadPrenotazioni(){
+  console.log("Caricamento prenotazioni...")
+  fetch("http://localhost:8080/api/v1/istruttori/prenotazioniIstruttore?token="+getCookie("token"),{
+
+      method:"GET",
+      headers: {
+          'Content-Type': 'application/json',
+          "Access-Control-Allow-Origin": "*"
+        }
+  })
+  .then((res)=>{
+      console.log("TOKEN:", getCookie("token"))
+      res.json()
+      .then((el)=>{
+          if(res.status==404 || res.status==400){
+              alert("Errore: impossibile caricare dati per questo ID");
+              return;
+          }
+          var tbodyRef = document.getElementById('tab').getElementsByTagName('tbody')[0];
+          el.prenotazioni_istruttore.forEach(prenot => {
+              var id_prenot = prenot._id
+              var data = prenot.slot.substring(0, 10)
+              var ora = prenot.slot.substring(11, 16)
+              var nome_studente = prenot.nominativo_studente 
+
+              // each prenotation in a new raw
+              var newRow = tbodyRef.insertRow();
+
+              // data
+              newCell = newRow.insertCell();
+              newText = document.createTextNode(data);
+              newCell.appendChild(newText);
+
+              // ora
+              newCell = newRow.insertCell();
+              newText = document.createTextNode(ora);
+              newCell.appendChild(newText);
+
+              // nome studente
+              newCell = newRow.insertCell();
+              newText = document.createTextNode(nome_studente);
+              newCell.appendChild(newText);
+
+              // presenza
+
+              newCell = newRow.insertCell();
+              var checkbox = document.createElement('input');
+              checkbox.type = 'checkbox';
+              newCell.appendChild(checkbox);
+              if(prenot.presenza){
+                checkbox.checked = true
+              }
+              checkbox.addEventListener('change', function() {
+                if (this.checked) {
+                  console.log("Checkbox is checked..");
+                } else {
+                  console.log("Checkbox is not checked..");
+                  console.log("Deleting prenotazione ", id_prenot)
+                }
+                // fetch operation
+                fetch("http://localhost:8080/api/v1/prenotazioni/modificaPresenza?token="+getCookie("token")+"&id_guida=" + id_prenot,{
+                  method:"POST",
+                  headers: {
+                      'Content-Type': 'application/json',
+                      "Access-Control-Allow-Origin": "*"
+                    },
+                  })
+                  .then(res=>{
+                      if(res.status==401 || res.status==404){
+                          console.log("Errore:", res.error)   // errore
+                          //alert("Errore:", res)
+                          return;
+                      }
+                  })
+                  .catch(err=>alert(err))
+                }
+              );
+          });
+      })
+      .catch(err=>alert(err));
+  })
+  .catch(err=>alert(err));
+
 }
 
 
