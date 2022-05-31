ae26640898472440abc47b5623f5a61bad72b569
simonescevaroli <simonescevaroli@gmail.com>
2022-05-31 08:51:58 +0200

    added test case

--- app/istruttori.test.js
+++ app/istruttori.test.js
@@ -1,2 +1,14 @@
 const request = require('supertest'); const app = require('./app');
 const jwt = require('jsonwebtoken'); const mongoose = require('mongoose');
+
+describe("GET /api/v1/istruttori/visualizzaImpegni", ()=>{
+
+});
+
+describe("GET /api/v1/istruttori/verificaDiponibilita", ()=>{
+
+});
+
+describe("GET /api/v1/istruttori/prenotazioniIstruttore", ()=>{
+
+});
