e0616ca2b4b697c65fdf36f4de32694e8a9f2aab
Andrea Lorenzetti <lorenzetti889@gmail.com>
2022-05-31 08:51:13 +0200

    skeleton code of authentications prenotations secretariat created

--- app/prenotazioni.test.js
+++ app/prenotazioni.test.js
@@ -1,2 +1,19 @@
 const request = require('supertest'); const app = require('./app');
 const jwt = require('jsonwebtoken'); const mongoose = require('mongoose');
+
+
+describe("POST /api/v1/prenotazioni/prenotaGuida",()=>{
+
+})
+
+describe("DELETE /api/v1/prenotazioni/annullaGuida",()=>{
+
+})
+
+describe("GET /api/v1/prenotazioni/mieGuide",()=>{
+
+})
+
+describe("GET /api/v1/prenotazioni/modificaPresenza",()=>{
+
+})
