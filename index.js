d70d5c21e644b28000b734b9fa1a2d9c0da33ed4
andrea-lorenzetti <99673279+andrea-lorenzetti@users.noreply.github.com>
2022-06-02 08:54:50 +0200

    change port from 8080 to 8000

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('./app/app.js');
 const mongoose = require('mongoose');
 
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8000;
 const DB_URL= process.env.DB_URL || 'mongodb://localhost:27017'
 console.log("DB_URL: "+DB_URL);
 // connect to mongodb locally 
